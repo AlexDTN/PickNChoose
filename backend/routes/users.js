5bb30c1eff99155590e23e2f228f7d93016e3a76
hyv3jz <112506352+hyv3jz@users.noreply.github.com>
2023-07-21 15:50:42 -0400

    added post request to /users/add

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,3 +7,18 @@ router.route('/').get((req, res) => {
     .then(users => res.json(users)) // return users in JSON format
     .catch(err => res.status(400).json('Error: ' + err)); // return error if there is one
 });
+
+// POST request to /users/add
+router.route('/add').post((req, res) => {
+  const username = req.body.username; 
+  const password = req.body.password;
+  const email = req.body.email; 
+
+  const newUser = new User({username, password, email});
+
+  newUser.save() // save new user to MongoDB Atlas database
+    .then(() => res.json('User added!')) // return message if successful
+    .catch(err => res.status(400).json('Error: ' + err)); // return error if there is one
+});
+
+module.exports = router;
