63e76b3ec9c465f3f4c9ac8ab4c13e7fb11dac04
hyv3jz <112506352+hyv3jz@users.noreply.github.com>
2023-07-21 16:35:31 -0400

    added POST request to UPDATE user information

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,14 +5,14 @@ let User = require('../models/user.model');
 router.route('/').get((req, res) => {
   User.find() // find all users in the MongoDB Atlas database
     .then(users => res.json(users)) // return users in JSON format
-    .catch(err => res.status(400).json('Error: ' + err)); // return error if there is one
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 // GET request to /users/:id
 router.route('/:id').get((req, res) => {
   User.findById(req.params.id) // find user with the given id
     .then(user => res.json(user)) // return user in JSON format
-    .catch(err => res.status(400).json('Error: ' + err)); // return error if there is one
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 // POST request to /users/add
@@ -24,17 +24,31 @@ router.route('/add').post((req, res) => {
   const newUser = new User({username, password, email});
 
   newUser.save() // save new user to MongoDB Atlas database
-    .then(() => res.json('User added!')) // return message if successful
-    .catch(err => res.status(400).json('Error: ' + err)); // return error if there is one
+    .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 // DELETE request to /users/:id
 router.route('/:id').delete((req, res) => {
   User.findByIdAndDelete(req.params.id) // find user with the given id and delete
-    .then(() => res.json('User deleted.')) // return message if successful
-    .catch(err => res.status(400).json('Error: ' + err)); // return error if there is one
+    .then(() => res.json('User deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
+// POST request to /users/update/:id
+router.route('/update/:id').post((req, res) => {
+  User.findById(req.params.id) // find user with the given id
+    .then(user => {
+      user.username = req.body.username;
+      user.password = req.body.password;
+      user.email = req.body.email; 
+
+      user.save()
+        .then(() => res.json('User updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
 
 
 
