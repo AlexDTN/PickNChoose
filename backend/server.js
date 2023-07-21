40e75642834c309873dd1fa426bff2085afac173
hyv3jz <112506352+hyv3jz@users.noreply.github.com>
2023-07-21 15:19:48 -0400

    added connection to mongodb atlas database

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const databaseName = 'backend-trial';
+const uri = process.env.ATLAS_URI;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log(`MongoDB database connected successfully`))
+  .catch((err) => console.error('Error connecting to MongoDB: ' + err));
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
