a3019df8dc2c422c8a9342586fa3600b7820b3d9
hyv3jz <112506352+hyv3jz@users.noreply.github.com>
2023-07-21 15:37:30 -0400

    added GET request for users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,2 +1,9 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
+
+// GET request to /users/
+router.route('/').get((req, res) => {
+  User.find() // find all users in the MongoDB Atlas database
+    .then(users => res.json(users)) // return users in JSON format
+    .catch(err => res.status(400).json('Error: ' + err)); // return error if there is one
+});
